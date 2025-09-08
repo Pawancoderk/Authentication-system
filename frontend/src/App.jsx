ab4c11fdb38e8a67e6b46d5da3be5c0748c00bb3
Pawan <imrealpawan@gmail.com>
2025-09-08 13:03:24 +0530

    feat: create initial structure for frontend with main components and routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+ return(
+  <div className='text-green-400'>App</div>
+ )
+}
+
+export default App
