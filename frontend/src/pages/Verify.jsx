342111cff64ac8a65df8f7742518ed56933bcb7e
Pawan <imrealpawan@gmail.com>
2025-09-19 20:30:38 +0530

    feat(verify): implement user verification with loading state and success/error messages
    - Add user verification logic using axios to post verification token
    - Display loading state while verification is in progress
    - Show success or error messages based on verification result
    - Remove StrictMode wrapper

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,8 +1,41 @@
+import axios from 'axios'
 import React from 'react'
+import { server } from '../main'
+import { useParams } from 'react-router-dom'
+import Loading from '../loading'
+import { useState, useEffect } from 'react'
 
 const Verify = () => {
+  const [sucessMessage, setSucessMessage] = useState("")
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const param = useParams()
+
+  const [loading, setLoading] = useState(true)
+
+  async function verifyUser() {
+    try {
+      const { data } = await axios.post(`${server}/api/v1/verify/${param.token}`)
+      setSucessMessage(data.message)
+    } catch (error) {
+      setErrorMessage("Verification link expired")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(()=>{
+   verifyUser()
+  },[])
   return (
-    <div>Verify</div>
+    <>
+      {
+        loading ? <Loading /> : <div className='w-[200px] m-auto mt-12-'>
+          {sucessMessage && <p className='text-green-500 text-2xl'>{sucessMessage} </p>}
+          {errorMessage && <p className='text-red-500 text-2xl'>{errorMessage} </p>}
+        </div>
+      }
+    </>
   )
 }
 
